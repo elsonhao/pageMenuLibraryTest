0efac9ec94aa143901aaf5aa22cf796d4d76b4e2
elson Huang <elson.huang@jamzoo.com.tw>
2017-07-17 13:44:29 +0800

    finish pageMenu

--- pageMenuTest/ViewController.swift
+++ pageMenuTest/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import PageMenu
 
 class ViewController: UIViewController {
 
+    
+    var myPageMenu:CAPSPageMenu?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var controllerArray : [UIViewController] = []
+        
+        let controller1 : UIViewController = UIViewController()
+        controller1.view.backgroundColor = .red
+        controller1.title = "Red"
+        controllerArray.append(controller1)
+        
+        let controller2 : UIViewController = UIViewController()
+        controller2.view.backgroundColor = .orange
+        controller2.title = "Orange"
+        controllerArray.append(controller2)
+        
+        let controller3 : UIViewController = UIViewController()
+        controller3.view.backgroundColor = .green
+        controller3.title = "Green"
+        controllerArray.append(controller3)
+        
+        let controller4 : UIViewController = UIViewController()
+        controller4.view.backgroundColor = .blue
+        controller4.title = "Blue"
+        controllerArray.append(controller4)
+        
+        myPageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x: 0.0, y: 50.0, width: self.view.frame.width, height: 100), pageMenuOptions: nil)
+        
+        self.view.addSubview(myPageMenu!.view)
     }
 
     override func didReceiveMemoryWarning() {
